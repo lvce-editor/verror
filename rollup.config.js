1bae079a1960523b74c65801a6a04fa99e771885
Le Vivilet <levivlet@pm.me>
2024-04-01 09:34:45 +0200

    feature: use typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,21 @@
+import { babel } from '@rollup/plugin-babel'
+import pluginTypeScript from '@babel/preset-typescript'
+
 export default {
-  input: 'src/index.js',
+  input: 'src/index.ts',
+  preserveEntrySignatures: 'strict',
+  treeshake: {
+    propertyReadSideEffects: false,
+  },
   output: {
     file: 'dist/dist/index.js',
     format: 'es',
   },
+  plugins: [
+    babel({
+      babelHelpers: 'bundled',
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      presets: [pluginTypeScript],
+    }),
+  ],
 }

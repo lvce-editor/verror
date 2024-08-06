b916a981f68bbece96e6a48ac6c149fd59557b52
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-06 20:02:58 +0200

    feature: format code (#35)

--- test/GetCombinedMessage.test.ts
+++ test/GetCombinedMessage.test.ts
@@ -4,12 +4,15 @@ import * as GetCombinedMessage from '../src/parts/GetCombinedMessage/GetCombined
 test('getCombinedMessage - no message', () => {
   const error = new TypeError('x is not a function')
   const message = ''
-  expect(GetCombinedMessage.getCombinedMessage(error, message)).toBe('TypeError: x is not a function')
+  expect(GetCombinedMessage.getCombinedMessage(error, message)).toBe(
+    'TypeError: x is not a function',
+  )
 })
-
 
 test('getCombinedMessage - with message', () => {
   const error = new TypeError('x is not a function')
   const message = 'Failed to execute function'
-  expect(GetCombinedMessage.getCombinedMessage(error, message)).toBe('Failed to execute function: TypeError: x is not a function')
+  expect(GetCombinedMessage.getCombinedMessage(error, message)).toBe(
+    'Failed to execute function: TypeError: x is not a function',
+  )
 })

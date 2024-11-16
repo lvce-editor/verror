ec39b0efd714473ba1c60f3ebbe1e933d2b1c971
Le Vivilet <levivlet@pm.me>
2024-11-16 12:31:47 +0100

    feature: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    ignores: [
+      'dist',
+      '.tmp',
+      '**/build/**',
+      '**/coverage/**',
+      '**/server/**',
+      '**/e2e/**',
+      'scripts',
+      'rollup.config.js',
+      'eslint.config.js',
+      'packages/text-search-worker/src/textSearchWorkerMain.ts',
+    ],
+  },
+  {
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/consistent-type-exports': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      'no-case-declarations': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+    },
+  },
+)
